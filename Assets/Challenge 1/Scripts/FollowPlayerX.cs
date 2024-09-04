6a260a03d8a7bcaf2b902adbe626e1092e26f235
LaurendaA <alaurenda2@gmail.com>
2024-09-04 15:57:06 +0100

    Correct FollowPlayerX script
    Correct FollowPlayerX script

--- Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -10,12 +10,14 @@ public class FollowPlayerX : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        // Attach phe plane GameObject to the camera
+        plane = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Make the camera following the plane
         transform.position = plane.transform.position + offset;
     }
 }
